b46420f3ce07f7e72637947029ff839320f993c8
hirunefu <280693590+hirunefu@users.noreply.github.com>
2026-04-28 14:02:14 +0900

    jest: ExpoのlazyグローバルをsetupFilesでeager化（Jest30 teardownエラー対策）

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,5 @@ module.exports = {
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg))'
   ],
   testMatch: ['**/?(*.)+(test).[jt]s?(x)'],
+  setupFiles: ['<rootDir>/jest.setup.warm-expo-globals.js'],
 };
